164fa3e3e035d5ee360460be730ad79aa50a02b0
enigmango <t.wengerd@gmail.com>
2015-01-11 14:15:34 -0500

    testing // instead of http:// in nodejs

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.tylerwengerd.com',
+        url: '//www.tylerwengerd.com',
         mail: {
             transport: 'SMTP',
                 options: {
