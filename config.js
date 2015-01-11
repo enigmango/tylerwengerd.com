fddfed81af748153897a90872ea82b0a8e8df9e5
enigmango <t.wengerd@gmail.com>
2015-01-11 14:28:43 -0500

    Revert "testing // instead of http:// in nodejs"
    This reverts commit 164fa3e3e035d5ee360460be730ad79aa50a02b0.
    
    http:// or https:// must be used

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: '//www.tylerwengerd.com',
+        url: 'http://www.tylerwengerd.com',
         mail: {
             transport: 'SMTP',
                 options: {
