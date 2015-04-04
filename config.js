d8dc596dae9af2666612a8d473ce6247d3145612
Tyler Wengerd <hi@tylerwengerd.com>
2015-04-04 23:06:43 +0000

    move to digitalocean, tweaks for new db

--- config.js
+++ config.js
@@ -6,10 +6,42 @@ var path = require('path'),
     config;
 
 config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: 'https://www.tylerwengerd.com',
+        mail: {
+            transport: 'SMTP',
+                options: {
+                    service: 'Mailgun',
+                    auth: {
+                        user: process.env.MAILGUN_SMTP_LOGIN,
+                        pass: process.env.MAILGUN_SMTP_PASSWORD
+                    }
+                }
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        }
+    },
+
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        // Change this to your Ghost blogs published URL.
+        url: 'http://localhost:2368',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
@@ -42,43 +74,6 @@ config = {
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
-    },
-
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://www.tylerwengerd.com',
-        mail: {
-            transport: 'SMTP',
-                options: {
-                    service: 'Mailgun',
-                    auth: {
-                        user: process.env.MAILGUN_SMTP_LOGIN,
-                        pass: process.env.MAILGUN_SMTP_PASSWORD
-                    }
-                }
-        },
-        database: {  
-            client: 'postgres',
-            connection: {
-                host: process.env.POSTGRES_HOST,
-                user: process.env.POSTGRES_USER,
-                password: process.env.POSTGRES_PASS,
-                database: process.env.POSTGRES_DB,
-                port: process.env.POSTGRES_PORT
-            }
-        },
-        aws: {  
-            accessKeyId: process.env.AWS_ACCESS_ID,
-            secretAccessKey: process.env.AWS_ACCESS_KEY,
-            bucket: process.env.AWS_BUCKET_NAME,
-            region: process.env_AWS_BUCKET_REGION
-        },
-        server: {  
-            host: '0.0.0.0',
-            port: process.env.PORT
-        }   
     },
 
     // **Developers only need to edit below here**
